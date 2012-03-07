89f3c21880961ccd52c1233e019b2a98d2ae88ab
Tatu Saloranta <tsaloranta@gmail.com>
2012-03-07 09:05:50 -0800

    ...

--- src/main/java/com/fasterxml/jackson/datatype/joda/deser/PeriodDeserializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/joda/deser/PeriodDeserializer.java
@@ -11,22 +11,22 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 
 public class PeriodDeserializer
-        extends JodaDeserializerBase<ReadablePeriod>
-    {
-       public PeriodDeserializer() { super(ReadablePeriod.class); }
+    extends JodaDeserializerBase<ReadablePeriod>
+{
+    public PeriodDeserializer() { super(ReadablePeriod.class); }
    
-       @Override
-       public ReadablePeriod deserialize(JsonParser jp, DeserializationContext ctxt)
-           throws IOException, JsonProcessingException
-       {
-           // TODO: perhaps support array of numbers...
-           //if (jp.isExpectedStartArrayToken()) { ]
-           switch (jp.getCurrentToken()) {
-           case VALUE_NUMBER_INT: // assume it's millisecond count
-               return new Period(jp.getLongValue());            
-           case VALUE_STRING:
-               return new Period(jp.getText());
-           }
-           throw ctxt.wrongTokenException(jp, JsonToken.START_ARRAY, "expected JSON Number or String");
-       }
+    @Override
+    public ReadablePeriod deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException
+    {
+        // TODO: perhaps support array of numbers...
+        //if (jp.isExpectedStartArrayToken()) { ]
+        switch (jp.getCurrentToken()) {
+        case VALUE_NUMBER_INT: // assume it's millisecond count
+            return new Period(jp.getLongValue());            
+        case VALUE_STRING:
+            return new Period(jp.getText());
+        }
+        throw ctxt.wrongTokenException(jp, JsonToken.START_ARRAY, "expected JSON Number or String");
    }
+}
