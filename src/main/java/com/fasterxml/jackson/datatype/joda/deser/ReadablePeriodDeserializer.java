0f0e6ce223435227a3bfcf32484f5ff762dd02d9
Tatu <tsaloranta@gmail.com>
2014-05-21 16:27:48 -0700

    Cleaning up #40 implementation

--- src/main/java/com/fasterxml/jackson/datatype/joda/deser/ReadablePeriodDeserializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/joda/deser/ReadablePeriodDeserializer.java
@@ -1,67 +1,53 @@
 package com.fasterxml.jackson.datatype.joda.deser;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.TreeNode;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.node.IntNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import org.joda.time.Days;
-import org.joda.time.Hours;
-import org.joda.time.Minutes;
-import org.joda.time.Months;
-import org.joda.time.ReadablePeriod;
-import org.joda.time.Seconds;
-import org.joda.time.Weeks;
-import org.joda.time.Years;
-
 import java.io.IOException;
+
+import com.fasterxml.jackson.core.*;
+
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import org.joda.time.*;
 
 public class ReadablePeriodDeserializer extends JodaDeserializerBase<ReadablePeriod>
 {
-	public ReadablePeriodDeserializer()
-	{
-		super( ReadablePeriod.class );
-	}
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public ReadablePeriod deserialize( JsonParser jsonParser, DeserializationContext deserializationContext ) throws IOException, JsonProcessingException
-	{
-		TreeNode treeNode = jsonParser.getCodec().readTree( jsonParser );
-		String periodType = ((TextNode)treeNode.get( "fieldType" ).get( "name" )).textValue();
-		String periodName = ((TextNode)treeNode.get( "periodType" ).get( "name" )).textValue();
-		int periodValue = ((IntNode)treeNode.get( periodType )).intValue();
-		if (periodName.equals( "Seconds" ))
-		{
-			return Seconds.seconds( periodValue );
-		}
-		else if (periodName.equals( "Minutes" ))
-		{
-			return Minutes.minutes( periodValue );
-		}
-		else if (periodName.equals( "Hours" ))
-		{
-			return Hours.hours( periodValue );
-		}
-		else if (periodName.equals( "Days" ))
-		{
-			return Days.days( periodValue );
-		}
-		else if (periodName.equals( "Weeks" ))
-		{
-			return Weeks.weeks( periodValue );
-		}
-		else if (periodName.equals( "Months" ))
-		{
-			return Months.months( periodValue );
-		}
-		else if (periodName.equals( "Years" ))
-		{
-			return Years.years( periodValue );
-		}
-		else
-		{
-			return null;
-		}
-	}
+    public ReadablePeriodDeserializer() {
+        super(ReadablePeriod.class);
+    }
+
+    @Override
+    public ReadablePeriod deserialize(JsonParser jsonParser, DeserializationContext ctxt)
+        throws IOException
+    {
+        JsonNode treeNode = jsonParser.readValueAsTree();
+        String periodType = treeNode.path("fieldType").path("name").asText();
+        String periodName = treeNode.path("periodType").path("name").asText();
+        // any "weird" numbers we should worry about?
+        int periodValue = treeNode.path(periodType).asInt();
+        if (periodName.equals( "Seconds" )) {
+            return Seconds.seconds( periodValue );
+        }
+        if (periodName.equals( "Minutes" )) {
+            return Minutes.minutes( periodValue );
+        }
+        if (periodName.equals( "Hours" )) {
+            return Hours.hours( periodValue );
+        }
+        if (periodName.equals( "Days" )) {
+            return Days.days( periodValue );
+        }
+        if (periodName.equals( "Weeks" )) {
+            return Weeks.weeks( periodValue );
+        }
+        if (periodName.equals( "Months" )) {
+            return Months.months( periodValue );
+        }
+        if (periodName.equals( "Years" )) {
+            return Years.years( periodValue );
+        }
+        throw ctxt.mappingException("Don't know how to deserialize ReadablePeriod using periodName '"
+                +periodName+"'");
+    }
 }
