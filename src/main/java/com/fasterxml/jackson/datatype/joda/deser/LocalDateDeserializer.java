a0eea1bef19a1ec1c1f60bb15c883b05de2cad5d
Cowtowncoder <tatu.saloranta@iki.fi>
2015-05-27 11:00:20 -0700

    Update release notes wrt #62, minor tweak to patch

--- src/main/java/com/fasterxml/jackson/datatype/joda/deser/LocalDateDeserializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/joda/deser/LocalDateDeserializer.java
@@ -20,32 +20,38 @@ public class LocalDateDeserializer
     public LocalDateDeserializer() { super(LocalDate.class); }
 
     @Override
-    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException
+    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException
     {
-        // [yyyy,mm,dd] or ["yyyy","mm","dd"]
-        if (jp.isExpectedStartArrayToken()) {
-            jp.nextToken(); // VALUE_NUMBER_INT or VALUE_STRING
-            int year = new Integer(jp.getText());
-            jp.nextToken(); // VALUE_NUMBER_INT or VALUE_STRING
-            int month = new Integer(jp.getText());
-            jp.nextToken(); // VALUE_NUMBER_INT or VALUE_STRING
-            int day = new Integer(jp.getText());
-            if (jp.nextToken() != JsonToken.END_ARRAY) {
-                throw ctxt.wrongTokenException(jp, JsonToken.END_ARRAY, "after LocalDate ints");
-            }
-            return new LocalDate(year, month, day);
-        }
-        switch (jp.getCurrentToken()) {
-        case VALUE_NUMBER_INT:
-            return new LocalDate(jp.getLongValue());            
-        case VALUE_STRING:
-            String str = jp.getText().trim();
+        if (p.getCurrentToken() == JsonToken.VALUE_STRING) {
+            String str = p.getText().trim();
             if (str.length() == 0) { // [JACKSON-360]
                 return null;
             }
             return parser.parseLocalDate(str);
-        default:
         }
-        throw ctxt.wrongTokenException(jp, JsonToken.START_ARRAY, "expected JSON Array, String or Number");
+        if (p.getCurrentToken() == JsonToken.VALUE_NUMBER_INT) {
+            return new LocalDate(p.getLongValue());            
+        }
+        
+        // [yyyy,mm,dd] or ["yyyy","mm","dd"]
+        if (p.isExpectedStartArrayToken()) {
+            int year = p.nextIntValue(-1); // fast speculative case
+            if (year == -1) { // either -1, or not an integral number; slow path
+                year = _parseIntPrimitive(p, ctxt);
+            }
+            int month = p.nextIntValue(-1);
+            if (month == -1) {
+                month = _parseIntPrimitive(p, ctxt);
+            }
+            int day = p.nextIntValue(-1);
+            if (day == -1) {
+                day = _parseIntPrimitive(p, ctxt);
+            }
+            if (p.nextToken() != JsonToken.END_ARRAY) {
+                throw ctxt.wrongTokenException(p, JsonToken.END_ARRAY, "after LocalDate ints");
+            }
+            return new LocalDate(year, month, day);
+        }
+        throw ctxt.wrongTokenException(p, JsonToken.START_ARRAY, "expected String, Number or JSON Array");
     }
 }
