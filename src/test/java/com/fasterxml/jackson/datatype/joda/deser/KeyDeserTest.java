f6cd6e42e75f336d0f3cd76e291aab8309b490b5
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-03-27 20:57:36 -0700

    Add JDK9+ module info, update travis settings

--- src/test/java/com/fasterxml/jackson/datatype/joda/deser/KeyDeserTest.java
+++ src/test/java/com/fasterxml/jackson/datatype/joda/deser/KeyDeserTest.java
@@ -31,7 +31,7 @@ public class KeyDeserTest extends JodaTestBase
     public void testDateTimeKeyDeserialize() throws IOException {
 
         final String json = "{" + quote("1970-01-01T00:00:00.000Z") + ":0}";
-        final Map<DateTime, Long> map = MAPPER.readValue(json, new TypeReference<Map<DateTime, String>>() { });
+        final Map<DateTime,?> map = MAPPER.readValue(json, new TypeReference<Map<DateTime, String>>() { });
 
         assertNotNull(map);
         assertTrue(map.containsKey(DateTime.parse("1970-01-01T00:00:00.000Z")));
@@ -40,7 +40,7 @@ public class KeyDeserTest extends JodaTestBase
     public void testLocalDateKeyDeserialize() throws IOException {
 
         final String json = "{" + quote("2014-05-23") + ":0}";
-        final Map<LocalDate, Long> map = MAPPER.readValue(json, new TypeReference<Map<LocalDate, String>>() { });
+        final Map<LocalDate,?> map = MAPPER.readValue(json, new TypeReference<Map<LocalDate, String>>() { });
 
         assertNotNull(map);
         assertTrue(map.containsKey(LocalDate.parse("2014-05-23")));
@@ -49,14 +49,14 @@ public class KeyDeserTest extends JodaTestBase
     public void testLocalTimeKeyDeserialize() throws IOException {
 
         final String json = "{" + quote("00:00:00.000") + ":0}";
-        final Map<LocalTime, Long> map = MAPPER.readValue(json, new TypeReference<Map<LocalTime, String>>() { });
+        final Map<LocalTime,?> map = MAPPER.readValue(json, new TypeReference<Map<LocalTime, String>>() { });
         assertNotNull(map);
         assertTrue(map.containsKey(LocalTime.parse("00:00:00.000")));
     }
     public void testLocalDateTimeKeyDeserialize() throws IOException {
 
         final String json = "{" + quote("2014-05-23T00:00:00.000") + ":0}";
-        final Map<LocalDateTime, Long> map = MAPPER.readValue(json, new TypeReference<Map<LocalDateTime, String>>() { });
+        final Map<LocalDateTime,?> map = MAPPER.readValue(json, new TypeReference<Map<LocalDateTime, String>>() { });
         assertNotNull(map);
         assertTrue(map.containsKey(LocalDateTime.parse("2014-05-23T00:00:00.000")));
     }
