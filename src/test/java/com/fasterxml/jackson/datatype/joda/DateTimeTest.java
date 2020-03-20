e3096aa5480b40635bae20cff554d2c91a3b386f
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-03-19 19:52:02 -0700

    moving to 2.11.0.rc1-SNAPSHOT

--- src/test/java/com/fasterxml/jackson/datatype/joda/DateTimeTest.java
+++ src/test/java/com/fasterxml/jackson/datatype/joda/DateTimeTest.java
@@ -158,7 +158,8 @@ public class DateTimeTest extends JodaTestBase
 
         String expectedJSON =
             "{\"jodaDateTime\":\"2000-01-01T00:00:00.000Z\","
-                + "\"javaUtilDate\":\"2000-01-01T00:00:00.000+0000\"}";
+                // 19-Mar-2020, tatu: [databind#2167] changed this to...
+                + "\"javaUtilDate\":\"2000-01-01T00:00:00.000+00:00\"}";
         String actualJSON = mapper.writeValueAsString(expectedBean);
         assertEquals(actualJSON, expectedJSON);
 
