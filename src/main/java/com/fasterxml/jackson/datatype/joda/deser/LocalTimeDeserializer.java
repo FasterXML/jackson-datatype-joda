4167481e5a22b1991e5d7b35510d63ba80f9a775
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-09-30 11:26:34 -0700

    refactoring, wrt LocalTime

--- src/main/java/com/fasterxml/jackson/datatype/joda/deser/LocalTimeDeserializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/joda/deser/LocalTimeDeserializer.java
@@ -39,39 +39,57 @@ public class LocalTimeDeserializer
         case JsonTokenId.ID_NUMBER_INT:
             return new LocalTime(p.getLongValue());            
         case JsonTokenId.ID_STRING:
-            String str = p.getText().trim();
-            if (str.length() == 0) {
-                return getNullValue(ctxt);
-            }
-            // 14-Jul-2020: [datatype-joda#117] Should allow use of "Timestamp as String" for
-            //     some textual formats
-            if (ctxt.isEnabled(StreamReadCapability.UNTYPED_SCALARS)
-                    && _isValidTimestampString(str)) {
-                return new LocalTime(NumberInput.parseLong(str));
-            }
-            return _format.createParser(ctxt).parseLocalTime(str);
+            return _fromString(p, ctxt, p.getText());
+        case JsonTokenId.ID_START_OBJECT:
+            return _fromString(p, ctxt,
+                    ctxt.extractScalarFromObject(p, this, handledType()));
         default:
         }
         // [HH,MM,ss,ms?]
         if (p.isExpectedStartArrayToken()) {
-            p.nextToken(); // VALUE_NUMBER_INT 
-            int hour = p.getIntValue(); 
-            p.nextToken(); // VALUE_NUMBER_INT
-            int minute = p.getIntValue();
-            p.nextToken(); // VALUE_NUMBER_INT
-            int second = p.getIntValue();
-            p.nextToken(); // VALUE_NUMBER_INT | END_ARRAY
-            int millis = 0;
-            if (p.currentToken() != JsonToken.END_ARRAY) {
-                millis = p.getIntValue();
-                p.nextToken(); // END_ARRAY?
-            }
-            if (p.currentToken() != JsonToken.END_ARRAY) {
-                throw ctxt.wrongTokenException(p, handledType(), JsonToken.END_ARRAY, "after LocalTime ints");
-            }
-            return new LocalTime(hour, minute, second, millis);
+            return _fromArray(p, ctxt);
         }
         return (LocalTime) ctxt.handleUnexpectedToken(handledType(), p.currentToken(), p,
                 "expected JSON Array, String or Number");
+    }
+
+    // @since 2.12
+    public LocalTime _fromString(final JsonParser p, final DeserializationContext ctxt,
+            String value)
+        throws IOException
+    {
+        value = value.trim();
+        if (value.isEmpty()) {
+            return getNullValue(ctxt);
+        }
+        // 14-Jul-2020: [datatype-joda#117] Should allow use of "Timestamp as String" for
+        //     some textual formats
+        if (ctxt.isEnabled(StreamReadCapability.UNTYPED_SCALARS)
+                && _isValidTimestampString(value)) {
+            return new LocalTime(NumberInput.parseLong(value));
+        }
+        return _format.createParser(ctxt).parseLocalTime(value);
+    }
+
+    // @since 2.12
+    public LocalTime _fromArray(final JsonParser p, final DeserializationContext ctxt)
+        throws IOException
+    {
+        p.nextToken(); // VALUE_NUMBER_INT 
+        int hour = p.getIntValue(); 
+        p.nextToken(); // VALUE_NUMBER_INT
+        int minute = p.getIntValue();
+        p.nextToken(); // VALUE_NUMBER_INT
+        int second = p.getIntValue();
+        p.nextToken(); // VALUE_NUMBER_INT | END_ARRAY
+        int millis = 0;
+        if (p.currentToken() != JsonToken.END_ARRAY) {
+            millis = p.getIntValue();
+            p.nextToken(); // END_ARRAY?
+        }
+        if (p.currentToken() != JsonToken.END_ARRAY) {
+            throw ctxt.wrongTokenException(p, handledType(), JsonToken.END_ARRAY, "after LocalTime ints");
+        }
+        return new LocalTime(hour, minute, second, millis);
     }
 }
