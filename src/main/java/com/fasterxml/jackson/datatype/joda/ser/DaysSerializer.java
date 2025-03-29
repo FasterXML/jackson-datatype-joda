236b477edf0f76269308a3a45d4329f746fd7855
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-03-30 02:00:06 +0900

    Fix #160 Add DaysSerializer (#161)

---
+++ src/main/java/com/fasterxml/jackson/datatype/joda/ser/DaysSerializer.java
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.datatype.joda.ser;
+
+import java.io.IOException;
+
+import org.joda.time.Days;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+/**
+ * Serializer for Joda {@link Days} class.
+ *
+ * @since 2.19
+ */
+public class DaysSerializer
+    extends JodaSerializerBase<Days>
+{
+    private static final long serialVersionUID = 1L;
+
+    public DaysSerializer() {
+        super(Days.class);
+    }
+
+    @Override
+    public void serialize(Days value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeNumber(value.getDays());
+    }
+}
