ad529f6d96b2cc4ed7b07b42ec5f910d27ec8bc7
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-05-23 13:56:43 -0700

    ...

--- src/test/java/com/fasterxml/jackson/datatype/joda/ModuleSPIMetadataTest.java
+++ src/test/java/com/fasterxml/jackson/datatype/joda/ModuleSPIMetadataTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ServiceLoader;
 
 public class ModuleSPIMetadataTest
+    extends JodaTestBase
 {
     @Test
     void testModuleSPIMetadata() {
